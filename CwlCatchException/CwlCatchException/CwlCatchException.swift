075110e1c45af2a7906bd1189bc226e01eb6a8ff
Matt Gallagher <matt@zqueue.com>
2016-11-03 12:10:01 +1100

    Fixed issues with Swift master and added specific tests for NSException.catchException.

--- CwlCatchException/CwlCatchException/CwlCatchException.swift
+++ CwlCatchException/CwlCatchException/CwlCatchException.swift
@@ -20,13 +20,29 @@
 
 import Foundation
 
-// We can't simply cast to Self? in the catchInBlock method so we need this generic function wrapper to do the conversion for us. Mildly annoying.
-private func catchReturnTypeConverter<T: NSException>(_ type: T.Type, block: () -> Void) -> T? {
-	return catchExceptionOfKind(type, block) as? T
-}
-
-extension NSException {
-	public static func catchException(in block: () -> Void) -> Self? {
-		return catchReturnTypeConverter(self, block: block)
+#if false
+	// We can't simply cast to Self? in the catchInBlock method so we need this generic function wrapper to do the conversion for us. Mildly annoying.
+	private func catchReturnTypeConverter<T: NSException>(_ type: T.Type, block: () -> Void) -> T? {
+		return catchExceptionOfKind(type, block) as? T
 	}
-}
+
+	extension NSException {
+		public static func catchException(in block: () -> Void) -> Self? {
+			return catchReturnTypeConverter(self, block: block)
+		}
+	}
+#else
+	// As of Nov 2016, the Swift development snapshots don't seem to like the previous version.
+	// You can use this version if you prefer.
+	private func catchReturnTypeConverter<T: NSException>(_ instance: T, block: () -> Void) -> T? {
+		// Get the type from an *instance*, instead of a receiving the type directly
+		return catchExceptionOfKind(T.self, block) as? T
+	}
+
+	extension NSException {
+		public static func catchException(in block: () -> Void) -> Self? {
+			// Use a dummy instance of Self to provide the type
+			return catchReturnTypeConverter(self.init(), block: block)
+		}
+	}
+#endif
