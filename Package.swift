cbd49b88df3b29225e4032e96f862d018c41e2f0
Matt Gallagher <matt@zqueue.com>
2017-08-02 17:11:15 +1000

    Removed unnecessary code signing settings.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
 		Target(name: "CwlMachBadInstructionHandler")
 	],
 	dependencies: [
-		.Package(url: "https://github.com/mattgallagher/CwlCatchException.git", Version(1, 0, 2, prereleaseIdentifiers: ["beta", "1"])),
+		.Package(url: "https://github.com/mattgallagher/CwlCatchException.git", Version(1, 0, 2, prereleaseIdentifiers: ["beta", "2"])),
 	],
 	exclude: [
 		"Sources/CwlPreconditionTesting/Mach/CwlPreconditionTesting.h",
