37cacdff2c5395ef142e8b2fc3ad221c963f9db9
Matt Gallagher <matt@zqueue.com>
2018-11-06 11:24:40 +1100

    Trying to unify macOS, iOS and tvOS builds into a single framework.

--- Sources/CwlPreconditionTesting/include/CwlPreconditionTesting.h
+++ Sources/CwlPreconditionTesting/include/CwlPreconditionTesting.h
@@ -26,5 +26,13 @@ FOUNDATION_EXPORT double CwlPreconditionTestingVersionNumber;
 //! Project version string for CwlUtils.
 FOUNDATION_EXPORT const unsigned char CwlAssertingTestingVersionString[];
 
+#if TARGET_OS_OSX || TARGET_OS_IOS
+
 #import <CwlCatchException/CwlCatchException.h>
 #import "CwlMachBadInstructionHandler.h"
+
+#elif !TARGET_OS_TV
+
+#error Unsupported platform.
+
+#endif
